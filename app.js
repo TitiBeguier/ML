b616221c48df0b912909cf1e36b8651e8547c0e5
TitiBeguier <beguiercartalibro@gmail.com>
2023-07-19 02:15:59 -0300

    Commit inicial

---
+++ app.js
@@ -0,0 +1,20 @@
+//dependencia
+const express = require('express');
+const app = express();
+const path = require('path');
+
+//numero de puerto
+const PORT=2000
+app.listen(PORT);
+//archivos estaticos, culaquiera de los dos
+//app.use(express.static(path.resolve(__dirname,'./public')))
+app.use(express.static('public'));
+
+//rutas
+
+app.get('/',(req, res)=>{
+    res.sendFile(path.join(__dirname,'./views/home.html'))
+});
+app.get('/products',(req, res)=>{
+    res.sendFile(path.join(__dirname,'./views/products.html'))
+});
