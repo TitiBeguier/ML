feb1927734cf95296bc99656b03c6211c9b86eee
TitiBeguier <beguiercartalibro@gmail.com>
2023-07-31 18:26:48 -0300

    hola

--- app.js
+++ app.js
@@ -15,3 +15,6 @@ app.use(express.static('public'));
 app.get('/',(req, res)=>{
     res.sendFile(path.join(__dirname,'./views/home.html'))
 });
+app.get('/registro',(req, res)=>{
+    res.sendFile(path.join(__dirname,'./views/registro.html'))
+});
